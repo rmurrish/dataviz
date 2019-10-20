747e02d37dda7d3451fdd8be93e9e0367185509c
rmurrish <rmurrish@users.noreply.github.com>
2019-10-19 17:11:30 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,24 +16,25 @@ class Donut {
     this.y = random(height);
     this.insideRadius = random(height);
     this.outsideRadius = random(width);
+    this.angle = random(360);
   }
 
 
   display() {  
     let numPoints = 50;
     let angle = 0;
-    let angleStep = random(360) / numPoints;
+    let angleStep = this.angle / numPoints;
     
     beginShape(QUAD_STRIP);
     for (let i = 0; i <= numPoints; i++) {
       let px = this.x + cos(radians(angle)) * this.outsideRadius;
       let py = this.y + sin(radians(angle)) * this.outsideRadius;
-      angle += angleStep;
+      //angle += angleStep;
       vertex(px, py);
       px = this.x + cos(radians(angle)) * this.insideRadius;
       py = this.y + sin(radians(angle)) * this.insideRadius;
       vertex(px, py);
-      //angle += angleStep;
+      angle += angleStep;
     }
     endShape();
   }
